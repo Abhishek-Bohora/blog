8133f4466f620839d08305641752946d148b7177
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-05-13 12:06:26 +0545

    signin api

---
+++ app/api/signin/route.ts
@@ -0,0 +1,27 @@
+import { signJwtAccessToken } from "@/app/lib/jwt";
+import { db } from "@/app/lib/db";
+import * as bcrypt from "bcrypt";
+interface RequestBody {
+  username: string;
+  password: string;
+}
+
+export async function POST(request: Request) {
+  const body: RequestBody = await request.json();
+
+  const user = await db.user.findFirst({
+    where: {
+      email: body.username,
+    },
+  });
+
+  if (user && (await bcrypt.compare(body.password, user.password))) {
+    const { password, ...userWithoutPass } = user;
+    const accessToken = signJwtAccessToken(userWithoutPass);
+    const result = {
+      ...userWithoutPass,
+      accessToken,
+    };
+    return new Response(JSON.stringify(result));
+  } else return new Response(JSON.stringify(null));
+}
